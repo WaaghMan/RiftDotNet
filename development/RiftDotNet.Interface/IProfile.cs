fdf2d025484965176d816e5c9391731e59598487
George Papadopoulos <virusfree21@gmail.com>
2013-10-13 22:10:34 +0300

    Adding missing files (thanks to pablocm)

---
+++ development/RiftDotNet.Interface/IProfile.cs
@@ -0,0 +1,18 @@
+﻿namespace RiftDotNet
+{
+	/// <summary>
+	/// ...
+	/// </summary>
+    public interface IProfile
+	{
+		/// <summary>
+        /// The height of the user in meters
+		/// </summary>
+        float PlayerHeight { get; }
+
+		/// <summary>
+        ///Distance between eyes in meters
+		/// </summary>
+        float IPD { get; }
+	}
+}
---
+++ development/RiftDotNet.Interface/IProfileManager.cs
@@ -0,0 +1,16 @@
+﻿namespace RiftDotNet
+{
+	/// <summary>
+	///     DeviceManager maintains and provides access to devices supported by OVR, such as
+	///     HMDs and sensors. A single instance of DeviceManager is normally created at
+	///     program startup, allowing devices to be enumerated and created.
+	/// </summary>
+    public interface IProfileManager
+	{
+		/// <summary>
+		/// The list of all HMD devices, one for each HMD, currently
+		/// attached to this system.
+		/// </summary>
+        IProfile GetDeviceDefaultProfile { get; }
+	}
+}
